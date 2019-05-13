5ad1044174731edd00182bd8a57fff65eecb8777
M. Jr <monagjr@gmail.com>
2019-05-13 19:56:02 +0200

    Integrate Blade

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,7 +2,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+Use eftec\bladeone\BladeOne;
 
 class BaseController extends AbstractController
 {
+    protected $blade;
+
+    function __construct(string $bladeViewsDir, string $bladeCacheDir) {
+        $blade = new BladeOne($bladeViewsDir,$bladeCacheDir,BladeOne::MODE_AUTO);
+        $this->blade = $blade;
+    }
 }
